70823fd0ea8d4922865736e5beac31a61624db48
Gabriela Vespoli <gabiat3@hotmail.com>
2021-05-31 15:26:17 -0300

    feat: add modules loader

--- app/js/views/View.js
+++ app/js/views/View.js
@@ -1,8 +1,19 @@
-class View {
-    constructor(selector) {
-        this._elemento = $(selector);
-    }
-    update(model) {
-        this._elemento.html(this.template(model));
-    }
-}
+System.register([], function (exports_1, context_1) {
+    "use strict";
+    var View;
+    var __moduleName = context_1 && context_1.id;
+    return {
+        setters: [],
+        execute: function () {
+            View = class View {
+                constructor(selector) {
+                    this._elemento = $(selector);
+                }
+                update(model) {
+                    this._elemento.html(this.template(model));
+                }
+            };
+            exports_1("View", View);
+        }
+    };
+});
